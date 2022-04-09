2c4af013ba4d4b188f6f35d992278b28fd3309fa
billybu10 <jakubb688@gmail.com>
2022-04-09 19:39:14 +0200

    ITA_woarg is done, now i'll make ITA

--- ITA_woarg.py
+++ ITA_woarg.py
@@ -4,23 +4,41 @@ from PIL import Image, ImageOps
 
 ASCII_CHARS = ["@", "#", "S", "%", "?", "*", "+", ";", ":", ",", "."]
 
-def main():
-    inputFile = input("Enter input file: ")
-    outputFile = input("Enter output file: ")
-    finalWidth = input("Enter output file width: ")
-    finalHeight = input("Enter output file height: ")
+def fun_readFile(inputFile):
     try:
         wsad = Image.open(inputFile)
     except:
         print("This path is invalid")
+    return wsad
+
+def fun_size(wsad, finalWidth, finalHeight):
     size = wsad.resize((finalWidth, finalHeight))
+    return size
+
+def fun_greysc(size):
     greysc = ImageOps.grayscale(size)
+    return greysc
+
+def fun_char(greysc, finalWidth):
     pixels = greysc.getdata()
     characters = "".join([ASCII_CHARS[pixel//25] for pixel in pixels])
-    pixel_count = len(characters)  
-    ascii_image = "\n".join([characters[index:(index+finalWidth)] for index in range(0, pixel_count, finalWidth)])
-    print(ascii_image)
+    pixelCount = len(characters)  
+    asciiImage = "\n".join([characters[index:(index+finalWidth)] for index in range(0, pixelCount, finalWidth)])
+    return asciiImage
+
+def fun_write(asciiImage, outputFile):
     with open(outputFile, "w") as f:
-        f.write(ascii_image)
+        f.write(asciiImage)
+
+def main():
+    inputFile = input("Enter input file: ")
+    outputFile = input("Enter output file: ")
+    finalWidth = int(input("Enter output file width: "), 10)
+    finalHeight = int(input("Enter output file height: "), 10)
+    asciiImage = fun_char(fun_greysc(fun_size(fun_readFile(inputFile), finalWidth, finalHeight)), finalWidth)
+    print(asciiImage)
+    fun_write(asciiImage, outputFile)
+
+    
     
 main()
